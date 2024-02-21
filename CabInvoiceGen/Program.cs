9e03afea61ad1b7271d078cae4815851a9fb284d
raghu <subamraghu11154@gmail.com>
2024-02-21 12:26:21 +0530

    Add project files.

---
+++ CabInvoiceGen/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("hii");
+    }
+}
+public class CabInvoice
+{
+    static double costPerKM = 10;
+    static double costPerMin = 10;
+    static double MinFare = 5;
+    public double TotalFare(double distance, int time)
+    {
+        double totval = (distance * costPerKM) + (time * costPerMin);
+        return Math.Max(totval, MinFare);
+    }
+}
