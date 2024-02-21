c8e82673a3d6a36a31b706de348c128a73e7782f
KuvvollaMurali123 <kuvvollamurali2@gmail.com>
2024-02-21 12:31:17 +0530

    testcases completed

--- CabInvoiceGen/Program.cs
+++ CabInvoiceGen/Program.cs
@@ -10,7 +10,7 @@ public class CabInvoice
     static double costPerKM = 10;
     static double costPerMin = 10;
     static double MinFare = 5;
-    public double TotalFare(double distance, int time)
+    public static sdouble TotalFare(double distance, int time)
     {
         double totval = (distance * costPerKM) + (time * costPerMin);
         return Math.Max(totval, MinFare);
